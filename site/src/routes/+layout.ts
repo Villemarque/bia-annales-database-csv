7968c8179625535277712a0ba93a3d8c9dfef511
Villemarque <villemarque@users.noreply.github.com>
2026-02-27 21:59:05 +0100

    resolve asset base url

--- site/src/routes/+layout.ts
+++ site/src/routes/+layout.ts
@@ -1,5 +1,8 @@
+import { asset } from '$app/paths';
 import type { LayoutLoad } from './$types';
+
 import { init } from '$lib/init';
+
 
 export const ssr = false; // SPA for PWA
 // TODO FIXME can this be actived back?
@@ -7,7 +10,7 @@ export const ssr = false; // SPA for PWA
 
 export const load: LayoutLoad = async ({ fetch }) => {
 	console.log('Loading questions in +layout.ts');
-	await fetch('/annales-bia.csv').then((response) =>
+	await fetch(asset('/annales-bia.csv')).then((response) =>
 		response.text().then((csv) => {
 			init(csv);
 		})
